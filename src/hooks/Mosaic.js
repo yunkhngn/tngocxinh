9be3df0780439e76bd721c9e78e852a787a79d88
yunkhngn <rogkhoa.mail@gmail.com>
2023-03-13 01:08:02 +0700

    Final

--- src/hooks/Mosaic.js
+++ src/hooks/Mosaic.js
@@ -1,12 +1,18 @@
 // import main from '../images/mosaic.png';
 import './Mosaic.css';
 import { MapInteractionCSS } from 'react-map-interaction';
-
 const Mosaic = () => {
     return (
-      <MapInteractionCSS>
+      <MapInteractionCSS
+        //default scale fit the screen responsively
+        defaultScale={window.innerWidth / 1920}
+        defaultPositionX={window.innerWidth / 2}
+        smoothScroll={true}
+        minScale={1}
+        maxScale={10}
+      >
         <div className="mosaic">
-           
+           <img src="https://i.imgur.com/ZckendN.jpg" alt="mosaic" />
         </div>
       </MapInteractionCSS>
     );
