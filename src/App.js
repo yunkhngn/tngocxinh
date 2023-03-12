0726a7bc3abd7834624c7998c691180046bcebfc
yunkhngn <rogkhoa.mail@gmail.com>
2023-03-13 00:32:38 +0700

    Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mosaic from './hooks/Mosaic';
+
+function App() {
+  return (
+    <div className="App">
+      <Mosaic />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/Mosaic.js
@@ -0,0 +1,15 @@
+// import main from '../images/mosaic.png';
+import './Mosaic.css';
+import { MapInteractionCSS } from 'react-map-interaction';
+
+const Mosaic = () => {
+    return (
+      <MapInteractionCSS>
+        <div className="mosaic">
+           
+        </div>
+      </MapInteractionCSS>
+    );
+}
+ 
+export default Mosaic;
